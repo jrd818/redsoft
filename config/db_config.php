b63dd963506d4024630383f17a19c6ee2f0ab860
liweifengbijiben <001@sijiuen.com>
2018-11-30 09:40:27 +0800

    pingban

--- config/db_config.php
+++ config/db_config.php
@@ -1,9 +1,8 @@
 <?php 
-    $mysql_sever_name='rm-bp1vu4v85gc19y27v.mysql.rds.aliyuncs.com';//内网地址
-     $mysql_sever_name='rm-bp1vu4v85gc19y27v0o.mysql.rds.aliyuncs.com';//外网地址
-    $mysql_username = 'yueyukeji';
-    $mysql_password = 'yueyukeji_66';
-    $mysql_database = 'redsoft';
+    $mysql_sever_name='192.168.0.158';//内网地址
+    $mysql_username = 'redsoft';
+    $mysql_password = 'redsoft';
+    $mysql_database = 'redsoft5200818';
     //设置浏览器字符集
     header("content-type:text/html;charset=utf-8");
     //建立链接

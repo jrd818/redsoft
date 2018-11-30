a90a089aef5a966c5754258c8cdb26abfd07db29
liweifengbijiben <001@sijiuen.com>
2018-11-30 11:07:01 +0800

    1120

--- config/db_config.php
+++ config/db_config.php
@@ -1,8 +1,7 @@
 <?php 
-    $mysql_sever_name='rm-bp1vu4v85gc19y27v.mysql.rds.aliyuncs.com';//内网地址
-     $mysql_sever_name='rm-bp1vu4v85gc19y27v0o.mysql.rds.aliyuncs.com';//外网地址
-    $mysql_username = 'yueyukeji';
-    $mysql_password = 'yueyukeji_66';
+    $mysql_sever_name='192.168.0.158';//内网地址
+    $mysql_username = 'redsoft';
+    $mysql_password = 'redsoft5200818';
     $mysql_database = 'redsoft';
     //设置浏览器字符集
     header("content-type:text/html;charset=utf-8");
